8b3a5b5a26fd4b017b1a91b128f026889ba0517e
Greutz <Gorton83@gmail.com>
2019-10-11 12:07:11 +0200

    Fixes mairi_christmas.rb, was looping get_townhall_urls too many times

--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -4,6 +4,7 @@ require 'open-uri'
 require 'pry'
 
 def open_links(arg)
+  puts "Veuillez patienter, scrapping en cours"
   # On récupère la sortie de get_townhalls_urls dans une variable full_url
 	full_url = get_townhall_urls
   emails_array = []
@@ -11,10 +12,11 @@ def open_links(arg)
   # Boucle ouvrant chaque liens de full_url
   full_url.each do |i|
     # On ouvre chaque lien avec Nokogiri et on va chercher les adresses mails avec xpath
-			new_url = Nokogiri::HTML(open(i))
+      new_url = Nokogiri::HTML(open(i))
+      puts "Processing #{i}"
         emails_array << new_url.xpath('//*[contains(text(), "@")]').text
   end
-    
+
   $town_names.each_with_index do |k, v| 
     result << {k => (emails_array)[v]}
   end
@@ -25,7 +27,7 @@ end
 
 def get_townhall_urls
   # On récupère l'url à parser
-	townhall_url = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/"))
+  townhall_url = Nokogiri::HTML(open("https://annuaire-des-mairies.com/95/"))
   url_list = []
   # Chemin ciblant tous les textes contenant "html"
     links_list = townhall_url.xpath('//*[contains(text(), "html")]')
@@ -37,13 +39,13 @@ def get_townhall_urls
 		full_url = []
     $town_names = []
     # Boucle créant deux array, l'un avec les noms des maires, l'autre avec les liens complets vers les pages contenant les adresses mail
-		url_list.each do |elem|
+    url_list.each do |elem|
       $town_names << elem[0..-5].to_sym
       # On ajoute #{nom_de_la_mairie.html} à "http://annuaire-des-mairies.com/95/" pour former les liens complets, stockés dans un array
-			full_url << "http://annuaire-des-mairies.com/95/#{elem}"
+			full_url << "https://www.annuaire-des-mairies.com/95/#{elem}"
     end
-		open_links($town_names)
   return full_url
 end
 
 get_townhall_urls
+open_links($town_names)
